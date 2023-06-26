0e19259c68b41b9edff3e8ca657d10f07851cf8e
anna-braga-jesus <annajesus.nave@gmail.com>
2023-06-26 10:41:32 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <title>Rango</title>
+</head>
+<body>
+    <header>
+        <h1> Rango - Gerador de receitas </h1>
+    </header>
+    <main>
+        <form>
+            <label for=""> Digite o que você tem na geladeira: </label>
+            <input type="text"/>
+            <input type="submit" value="Gere as receitas"/>
+        </form>
+        <p> Conteúdo principal </p>
+    </main>
+    <footer>
+        Anna Carolina Braga - 2023
+    </footer>
+    
+</body>
+</html>
